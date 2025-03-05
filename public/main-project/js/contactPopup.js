025993c4c2893c8b0e6eecdf38ff62059719d310
GiIIy <littlejjgill@gmail.com>
2025-03-05 22:43:51 +0700

    changed to free, and just ready for first launch!

---
+++ public/main-project/js/contactPopup.js
@@ -0,0 +1,11 @@
+
+// Feedback Popup Functions
+function openContact() {
+    document.getElementById('contactOverlay').style.display = 'block';
+    document.getElementById('contactPopup').style.display = 'block';
+  }
+  function closeContact() {
+    document.getElementById('contactOverlay').style.display = 'none';
+    document.getElementById('contactPopup').style.display = 'none';
+  }
+  
